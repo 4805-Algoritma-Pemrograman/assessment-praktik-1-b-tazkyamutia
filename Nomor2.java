9ba6496496f7f640893aeffa25dbf58f3686dff0
tazkyamutia <tazkyamr34@gmail.com>
2024-10-24 10:23:53 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class BungalowRental {
+public class Nomor2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
